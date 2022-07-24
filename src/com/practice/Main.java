5e0e0a77a1be2358446494513c74e34fd1a220b1
Geo3490 <geo40127@gmail.com>
2022-07-24 14:04:27 -0400

    update

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -1,7 +1,27 @@
 package com.practice;
 
+/*public class Main {
+    public static void main (String[] args){
+        byte georgeAge = 32;
+        String ownerName = "George";
+
+
+
+        System.out.println(georgeAge + "\n"+ ownerName);
+    }
+}*/
+
+                //EXAMPLE PRACTICE JULY 2022//
+//Use Primitive Types As Listed Below To Store Simple Values.
+
 public class Main {
     public static void main (String[] args){
-        System.out.println("My name is \n".);
+        byte age = 32;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+        System.out.println(letter+"\n" + price);
     }
 }
